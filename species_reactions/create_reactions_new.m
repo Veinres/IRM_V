26fab983e36c689b33b9d32f4327c194103efe4e
Veinres <v95713@gmail.com>
2026-08-07 17:41:31 +0000

    Correcting the rateOfRx again. R3 cold was wrong

--- species_reactions/create_reactions_new.m
+++ species_reactions/create_reactions_new.m
@@ -665,7 +665,7 @@ if ismember('Si',Spe.PSpeciess)
 [Reactionlist]=add_reaction({'eh','Si'},{'Siii','eh','e','e'},8,[5.44e-16 5.29e-19],'ion','Freund90',Reactionlist,'izhGSSii');
 
 % doubly ionization #2  — from single 
-[Reactionlist]=add_reaction({'e','Sii'},{'Siii','e','e'},1,[1.24e-15 0.736 27.884],'ion','Djuric93',Reactionlist,'izcSii');
+[Reactionlist]=add_reaction({'e','Sii'},{'Siii','e','e'},1,[1.24e-15 0.736 25.884],'ion','Djuric93',Reactionlist,'izcSii');
 [Reactionlist]=add_reaction({'eh','Sii'},{'Siii','eh','e'},8,[1.54e-14 9.50e-18],'ion','Djuric93',Reactionlist,'izhSii');
 
 % Ar Interaction
